b0b15314beeabb44fd97e8a4647160f7663dea5d
Stéphan <43352005+StephanJ98@users.noreply.github.com>
2020-09-11 14:13:02 +0200

    Update system.js

--- system.js
+++ system.js
@@ -1,20 +1,20 @@
 const os = require("os")
 
-console.log('Architecture:' + os.arch())
-console.log('CPU:' + os.cpus()[0].model + ' Nº Cores:' + os.cpus().length)
+console.log('Architecture: ' + os.arch())
+console.log('CPU: ' + os.cpus()[0].model + ' Nº Cores:' + os.cpus().length)
 console.log('Free RAM: ' + (os.freemem() / 1073741824).toPrecision(3) + 'Go')
 console.log('Total RAM: ' + (os.totalmem() / 1073741824).toPrecision(3) + 'Go')
 try {
     console.log('Network: Ethernet ' + JSON.stringify(os.networkInterfaces().Ethernet[1]))
 } catch (error) {
-    console.log('Network: No Ethernet')
+    console.log('Network: No Ethernet connection')
 }
 try {
     console.log('Network: WIFI ' + JSON.stringify(os.networkInterfaces()['Wi-Fi'][1]))
 } catch (error) {
-    console.log('Network: No WIFI')
+    console.log('Network: No WIFI connection')
 }
-console.log('Platform: ' + ' ' + os.type() + ' ' + os.platform() + ' ' + os.release() + ' ' + os.version())
+console.log('Platform: ' + os.type() + ' ' + os.platform() + ' ' + os.release() + ' ' + os.version())
 
 process.on('exit', (code) => {
     console.log('Process exit event with code: ', code);
