58da308dfe4706014ec02bdbd141e6e21b538379
Stéphan <43352005+StephanJ98@users.noreply.github.com>
2020-09-11 13:49:42 +0200

    Update system.js

--- system.js
+++ system.js
@@ -1,7 +1,7 @@
 const os = require("os")
 
 console.log('Architecture:' + os.arch())
-console.log('CPU:' + os.cpus()[0].model + 'Nº Cores:' + os.cpus().length)
+console.log('CPU:' + os.cpus()[0].model + ' Nº Cores:' + os.cpus().length)
 console.log('Free RAM: ' + (os.freemem() / 1073741824).toPrecision(3) + 'Go')
 console.log('Total RAM: ' + (os.totalmem() / 1073741824).toPrecision(3) + 'Go')
 try {
